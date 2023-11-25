fe19c576e28e6f3f6b9764602fec15aba25cd53f
Paul Hörmann <hoermannpaul@gmail.com>
2023-11-25 23:06:04 +0100

    create placeholders for initial components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NewGame />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NewGame from './components/NewGame.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NewGame
   }
 }
 </script>
